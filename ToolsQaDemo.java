31dd0be31ec49b8efd64d0a45ea0301d9358dce3
sabbirchow <sabbirchow@yahoo.com>
2017-12-23 20:47:56 -0500

    last revision - tools qa demo site

--- ToolsQaDemo.java
+++ ToolsQaDemo.java
@@ -1,10 +1,16 @@
 //import java.util.List;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -13,7 +19,7 @@ import org.testng.annotations.Test;
 public class ToolsQaDemo {
 
 	WebDriver driver;
-	String baseURl;
+	String baseURl = "http://toolsqa.com/automation-practice-form/";
 
 	@BeforeClass
 	public void startBrowser() {
@@ -25,7 +31,6 @@ public class ToolsQaDemo {
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		baseURl = "http://toolsqa.com/automation-practice-form/";
 
 	}
 
@@ -37,9 +42,9 @@ public class ToolsQaDemo {
 
 		// because on on my test page tab
 		// reference variable to test page
-		String testPageTab = driver.getWindowHandle();
+		// String testPageTab = driver.getWindowHandle();
 
-		System.out.println("Verifying homepage title...");
+		System.out.println("Verifying homepage title...\n");
 		String homepageTitle = driver.getTitle();
 		System.out.println("Current page title: " + homepageTitle);
 		Assert.assertEquals(homepageTitle,
@@ -51,14 +56,14 @@ public class ToolsQaDemo {
 		Assert.assertEquals(homepageURL,
 				"http://toolsqa.com/automation-practice-form/");
 
-		System.out.println("Verifying if the homepage image is showing");
+		System.out.println("Verifying if the homepage image is showing\n");
 
 		WebElement homePageImage = driver.findElement(By
 				.className("preload-me"));
 
 		boolean isHomepageisDisplayed = homePageImage.isDisplayed();
-		System.out
-				.println("Homepage image displayed: " + isHomepageisDisplayed);
+		System.out.println("Homepage image displayed: " + isHomepageisDisplayed
+				+ "\n");
 
 		// System.out.println("verifying social media bars");
 
@@ -66,11 +71,13 @@ public class ToolsQaDemo {
 
 		boolean isAddressEnabled = contactAddress.isEnabled();
 
-		System.out.println("Contact address enabled: " + isAddressEnabled);
+		System.out.println("Contact address enabled: " + isAddressEnabled
+				+ "\n");
 
 		if (isAddressEnabled == true) {
 
-			System.out.println("The location is " + contactAddress.getText());
+			System.out.println("The location is " + contactAddress.getText()
+					+ "\n");
 			// contactAddress.click();
 			// driver.navigate().back();
 			// Thread.sleep(5000);
@@ -81,11 +88,12 @@ public class ToolsQaDemo {
 
 		boolean isPhoneEnabled = contactPhone.isEnabled();
 
-		System.out.println("Phone number enabled: " + isPhoneEnabled);
+		System.out.println("Phone number enabled: " + isPhoneEnabled + "\n");
 
 		if (isPhoneEnabled == true) {
 
-			System.out.println("The phone number is " + contactPhone.getText());
+			System.out.println("The phone number is " + contactPhone.getText()
+					+ "\n");
 			// contactPhone.click();
 			// driver.navigate().back();
 			// Thread.sleep(5000);
@@ -95,11 +103,11 @@ public class ToolsQaDemo {
 
 		boolean isemailEnable = contactEmail.isEnabled();
 
-		System.out.println("Email enabled: " + isemailEnable);
+		System.out.println("Email enabled: " + isemailEnable + "\n");
 
 		if (isemailEnable == true) {
 
-			System.out.println("The email is " + contactEmail.getText());
+			System.out.println("The email is " + contactEmail.getText() + "\n");
 
 		}
 
@@ -108,18 +116,23 @@ public class ToolsQaDemo {
 
 		boolean isFacebookLinkEnabled = facebookHeaderLink.isEnabled();
 
-		System.out.println("Facebook header enabled: " + isFacebookLinkEnabled);
+		System.out.println("Facebook header enabled: " + isFacebookLinkEnabled
+				+ "\n");
 
 		if (isFacebookLinkEnabled == true) {
 
+			facebookHeaderLink.click();
+			Thread.sleep(5000);
 			// reference to facebook tab
 			String facebookTab = driver.getWindowHandle();
-			facebookHeaderLink.click();
-			Thread.sleep(2000);
-			driver.switchTo().window(facebookTab).close();
-			// driver.navigate().back();
-			driver.get(baseURl);
-			Thread.sleep(2000);
+
+			System.out.println("Facebook tab window handle value: "
+					+ facebookTab);
+
+			driver.switchTo().window(facebookTab);
+
+			Thread.sleep(5000);
+
 		}
 
 		WebElement twitterHeaderLink = driver.findElement(By
@@ -127,16 +140,21 @@ public class ToolsQaDemo {
 
 		boolean isTwitterHeaderLinkEnabled = twitterHeaderLink.isEnabled();
 		System.out.println("twitter header enabled: "
-				+ isTwitterHeaderLinkEnabled);
+				+ isTwitterHeaderLinkEnabled + "\n");
 
 		if (isTwitterHeaderLinkEnabled == true) {
 
-			String twitterTab = driver.getWindowHandle();
-			Thread.sleep(2000);
 			twitterHeaderLink.click();
-			driver.switchTo().window(twitterTab).close();
-			// driver.navigate().back();
-			driver.get(baseURl);
+
+			Thread.sleep(5000);
+
+			String twitterTab = driver.getWindowHandle();
+
+			System.out.println("Twitter tab window handle value: " + twitterTab
+					+ "\n");
+
+			driver.switchTo().window(twitterTab);
+
 			Thread.sleep(5000);
 
 		}
@@ -148,22 +166,101 @@ public class ToolsQaDemo {
 
 		if (isGoogleHeaderLinkEnabled == true) {
 
-			//reference tab for google
-			String googleTab = driver.getWindowHandle();
-			Thread.sleep(2000);
+			// reference tab for google
+			// String googleTab = driver.getWindowHandle();
+
 			googleHeaderLink.click();
-			driver.switchTo().window(googleTab).close();
-			driver.get(baseURl);
+
+			Thread.sleep(5000);
+
+			String googleTab = driver.getWindowHandle();
+
+			System.out.println("Google tab window handle value: " + googleTab
+					+ "\n");
+			// driver.switchTo().window(googleTab).close();
+			// driver.get(baseURl);
+
 			Thread.sleep(5000);
 
 		}
 
-		driver.switchTo().window(testPageTab);
+		// ///////////////////////////// START HERE TODAY
+		// /////////////////////////////////////
+
+		ArrayList<String> windowTabs = new ArrayList<String>(
+				driver.getWindowHandles());
+		driver.switchTo().window(windowTabs.get(3));
+		Thread.sleep(5000);
+
+		File facebookPageImage = ((TakesScreenshot) driver)
+				.getScreenshotAs(OutputType.FILE);
+		try {
+
+			org.apache.commons.io.FileUtils
+					.copyFile(
+							facebookPageImage,
+							new File(
+									"C:\\Users\\SABBIR CHOWDHURY\\Desktop\\DemoSiteImages\\facebookImage.png"));
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		driver.close();
+
+		driver.switchTo().window(windowTabs.get(2));
+		Thread.sleep(5000);
+
+		File TwitterPageImage = ((TakesScreenshot) driver)
+				.getScreenshotAs(OutputType.FILE);
+
+		try {
+			org.apache.commons.io.FileUtils
+					.copyFile(
+							TwitterPageImage,
+							new File(
+									"C:\\Users\\SABBIR CHOWDHURY\\Desktop\\DemoSiteImages\\twitterImage.png"));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		driver.close();
+
+		driver.switchTo().window(windowTabs.get(1));
+		Thread.sleep(5000);
+
+		File googlePageImage = ((TakesScreenshot) driver)
+				.getScreenshotAs(OutputType.FILE);
+
+		try {
+
+			org.apache.commons.io.FileUtils
+					.copyFile(
+							googlePageImage,
+							new File(
+									"C:\\Users\\SABBIR CHOWDHURY\\Desktop\\DemoSiteImages\\googleImage.png"));
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		driver.close();
+
+		driver.switchTo().window(windowTabs.get(0));
+
+		// driver.switchTo().window(testPageTab);
 
 		// number of tabs opened
-		driver.getWindowHandles().size();
 
-		// iver.switchTo().window(tabs ())
+		int numberOfTabsOpened = driver.getWindowHandles().size();
+
+		System.out.println("Number of tabs opened: " + numberOfTabsOpened
+				+ "\n");
+
+		Thread.sleep(2000);
 
 		WebElement firstNameInput = driver.findElement(By.name("firstname"));
 
@@ -171,20 +268,124 @@ public class ToolsQaDemo {
 
 		firstNameInput.sendKeys("John");
 
+		Thread.sleep(2000);
+
 		WebElement lastNameInput = driver.findElement(By.name("lastname"));
 
 		lastNameInput.clear();
 
 		lastNameInput.sendKeys("Doe");
 
+		Thread.sleep(2000);
 		/*
-		 * driver.switchTo().window(mainWindowHandle);
+		 * WebElement SexMale = driver.findElement(By.id("sex-0"));
+		 * SexMale.click(); Thread.sleep(2000);
+		 * 
+		 * System.out.println("Male is selected?" + SexMale.isSelected());
+		 * 
+		 * WebElement SexFemale = driver.findElement(By.id("sex-1"));
+		 * SexFemale.click(); Thread.sleep(2000);
+		 * 
+		 * System.out.println("Male is selected?" + SexFemale.isSelected());
+		 * 
+		 * /* driver.switchTo().window(mainWindowHandle);
 		 * driver.findElement(By.cssSelector("body")) .sendKeys(Keys.CONTROL +
 		 * "\t"); driver.switchTo().defaultContent();
 		 */
 		// for(int i = 0; i < navBar.size(); i++){
 
 		// }
+
+		boolean isChecked = false;
+
+		List<WebElement> yearExperienceRadio = driver
+				.findElements(By
+						.xpath("//input [contains (@type , 'radio') and contains (@name, 'exp')]"));
+
+		int size = yearExperienceRadio.size();
+
+		for (int i = 0; i < size; i++) {
+			isChecked = yearExperienceRadio.get(i).isSelected();
+
+			if (!isChecked) {
+				yearExperienceRadio.get(i).click();
+				Thread.sleep(2000);
+
+			}
+
+		}
+
+		WebElement datePicker = driver.findElement(By.id("datepicker"));
+
+		boolean isDatePickerDisplayed = datePicker.isDisplayed();
+
+		if (isDatePickerDisplayed == true) {
+
+			datePicker.clear();
+
+			datePicker.sendKeys("10/05/2020");
+
+		}
+
+		boolean isProfessionChecked = false;
+
+		List<WebElement> professionCheckBox = driver
+				.findElements(By
+						.xpath("//input[contains(@name, 'profession') and contains(@type, 'checkbox')]"));
+
+		int numberOfProfessions = professionCheckBox.size();
+
+		for (int i = 0; i < numberOfProfessions; i++) {
+
+			isProfessionChecked = professionCheckBox.get(i).isSelected();
+
+			if (!isProfessionChecked)
+				professionCheckBox.get(i).click();
+			Thread.sleep(2000);
+		}
+
+		boolean isAutomationChecked = false;
+
+		List<WebElement> automationCheckBoxes = driver
+				.findElements(By
+						.xpath("//input[contains(@name,'tool') and contains(@type,'checkbox')]"));
+
+		int numberofTools = automationCheckBoxes.size();
+
+		for (int i = 0; i < numberofTools; i++)
+
+		{
+			if (!isAutomationChecked)
+				automationCheckBoxes.get(i).click();
+			Thread.sleep(2000);
+		}
+
+		Select continentDropdown = new Select(driver.findElement(By
+				.id("continents")));
+
+		// List<Select> continentDropdown = new
+		// List<Select>(driver.findElements(By.id("continent")));
+
+		continentDropdown.selectByVisibleText("South America");
+
+		continentDropdown.selectByVisibleText("Europe");
+
+		WebElement submitButton = driver.findElement(By.id("submit"));
+
+		boolean isSubmitButtonEnabled = submitButton.isEnabled();
+
+		if (isSubmitButtonEnabled == true) {
+
+			System.out.println("Submit button is enalbed: "
+					+ isSubmitButtonEnabled);
+
+			submitButton.click();
+
+		} else {
+
+			System.out.println("Submit button is disabled at this time");
+
+		}
 
 	}
 
